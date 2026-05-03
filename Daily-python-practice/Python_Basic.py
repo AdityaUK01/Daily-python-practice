00fa8c6026beba07ca446fa530cf2251455b3a44
Aditya Rawat <108173854+AdityaUK01@users.noreply.github.com>
2026-05-03 18:41:51 +0530

    first day

---
+++ Daily-python-practice/Python_Basic.py
@@ -0,0 +1,37 @@
+print("hello World")
+
+#integer
+x = 5
+print(x)
+print(type(x))
+
+#float
+y = 3.14
+print(y)
+print(type(y))
+
+#string
+name = "Aditya"
+print(name)
+print(type(name))
+
+#boolean
+is_student = True
+print(is_student)
+print(type(is_student))
+
+# List
+fruits = ["apple", "banana" ,"cherry",1,2,3.14,True]
+print(fruits)
+print(type(fruits))
+
+#Tuple
+cooridinates = (10.0, 20.0)
+print(cooridinates)
+print(type(cooridinates))
+
+#Dictionory
+person = {"name": "Aditya", "age": 25}
+print(person)
+print(type(person))
+
