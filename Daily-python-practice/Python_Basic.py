0fe82b6bf269f7e3b66c53c46a85f10d0e8ee214
Aditya Rawat <108173854+AdityaUK01@users.noreply.github.com>
2026-05-08 14:36:29 +0530

    Daily

--- Daily-python-practice/Python_Basic.py
+++ Daily-python-practice/Python_Basic.py
@@ -162,4 +162,13 @@ class Person: #it is a class named Person
    
 
 
-#hhhhhhhhhhhh
+#Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"    
+print(even_or_odd(4)) #it will print "Even" because 4 is an even number
+print(even_or_odd(7)) #it will print "Odd" because 7    
+
---
+++ Daily-python-practice/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"    
+print(even_or_odd(4)) #it will print "Even" because 4 is an even number
+print(even_or_odd(7)) #it will print "Odd" because 7    
